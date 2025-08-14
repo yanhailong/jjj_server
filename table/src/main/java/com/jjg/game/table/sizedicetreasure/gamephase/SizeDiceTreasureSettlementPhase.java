e7d6c872a415d174a67cc4e5322e6875fca08a9c
chengchunlin <Ccl123!@#>
2025-08-14 19:52:52 +0800

    fix: 修复大小骰子个数bug

--- table/src/main/java/com/jjg/game/table/sizedicetreasure/gamephase/SizeDiceTreasureSettlementPhase.java
+++ table/src/main/java/com/jjg/game/table/sizedicetreasure/gamephase/SizeDiceTreasureSettlementPhase.java
@@ -38,7 +38,7 @@ public class SizeDiceTreasureSettlementPhase extends BaseDiceSettlementPhase<Siz
     public void phaseDoAction() {
         super.phaseDoAction();
         // 随机四个1-2的骰子点数
-        List<Integer> randomNumDice = DiceUtils.randomDice(4, 1, 2);
+        List<Integer> randomNumDice = DiceUtils.randomDice(3, 1, 6);
         // 通过骰子点数获取对应的配置
         List<WinPosWeightCfg> winPosWeightCfgs =
             DiceDataHolder.getWinPosWeightCfg(EGameType.SIZE_DICE_TREASURE, randomNumDice);
