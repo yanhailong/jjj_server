79e5f5e32968aef9ec8d766c5a49fcf1680a98a4
lm <1343955662@qq.com>
2025-08-20 15:25:32 +0800

    配置表提交

---
+++ sampledata/src/main/java/com/jjg/game/sampledata/container/StatusCfgContainer.java
@@ -0,0 +1,49 @@
+package com.jjg.game.sampledata.container;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.processing.Generated;
+import com.jjg.game.sampledata.bean.StatusCfg;
+
+/**
+ * status.xlsx配置管理容器
+ *
+ * @excelName status.xlsx
+ * @sheetName status
+ * @author auto_generator
+ */
+@Generated("com.eouna.configtool.generator.template.java.JavaTemplateGenerator")
+public class StatusCfgContainer extends BaseCfgContainer<StatusCfg> {
+
+  @Override
+  public boolean hasRelatedTable() {
+    return false;
+  }
+
+  @Override
+  public boolean isParentConfigNode() {
+    return false;
+  }
+
+  @Override
+  public StatusCfgContainer getNewContainer(){
+    return new StatusCfgContainer();
+  }
+
+  public StatusCfgContainer() {
+    super();
+  }
+
+  @Override
+  public List<String> getExcelNameList() {
+    List<String> excelNameList = new ArrayList<>();
+    excelNameList.add("status.xlsx");
+    return excelNameList;
+  }
+
+  @Override
+  protected StatusCfg createNewBean() {
+    return new StatusCfg();
+  }
+}
