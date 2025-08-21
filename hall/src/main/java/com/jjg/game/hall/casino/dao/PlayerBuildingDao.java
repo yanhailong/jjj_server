f7eed72c423b2872fb86b3deb73327d2e4dcb52b
lm <1343955662@qq.com>
2025-08-21 17:56:31 +0800

    我的赌场修改

--- hall/src/main/java/com/jjg/game/hall/casino/dao/PlayerBuildingDao.java
+++ hall/src/main/java/com/jjg/game/hall/casino/dao/PlayerBuildingDao.java
@@ -1,22 +1,39 @@
 package com.jjg.game.hall.casino.dao;
 
 import com.jjg.game.core.dao.MongoBaseDao;
+import com.jjg.game.hall.casino.data.CasinoInfo;
 import com.jjg.game.hall.casino.data.PlayerBuilding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author lm
  * @date 2025/8/18 09:38
  */
 @Repository
-public class PlayerBuildingDao extends MongoBaseDao<PlayerBuilding,Long> {
+public class PlayerBuildingDao extends MongoBaseDao<PlayerBuilding, Long> {
     public PlayerBuildingDao(@Autowired MongoTemplate mongoTemplate) {
         super(PlayerBuilding.class, mongoTemplate);
     }
 
-    public PlayerBuilding findById(long id) {
-        return mongoTemplate.findById(id, PlayerBuilding.class);
+    public List<PlayerBuilding> findByPlayerId(Long playerId) {
+        return mongoTemplate.find(
+                Query.query(Criteria.where("playerId").is(playerId)),
+                PlayerBuilding.class
+        );
     }
+
+    public PlayerBuilding findByPlayerIdAndCasinoId(Long playerId, int casinoId) {
+        return mongoTemplate.findOne(
+                Query.query(Criteria.where("playerId").is(playerId)
+                        .and("casinoId").is(casinoId)),
+                PlayerBuilding.class
+        );
+    }
+
 }
