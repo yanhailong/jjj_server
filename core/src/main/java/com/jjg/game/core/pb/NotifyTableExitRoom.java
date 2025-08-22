f66e149aadc0bec8b25429deab835fc1815077b5
lm <1343955662@qq.com>
2025-08-22 11:41:42 +0800

    通知退出房间协议修改

--- core/src/main/java/com/jjg/game/core/pb/NotifyTableExitRoom.java
+++ core/src/main/java/com/jjg/game/core/pb/NotifyTableExitRoom.java
@@ -1,20 +1,18 @@
-package com.jjg.game.table.common.message.req;
+package com.jjg.game.core.pb;
 
 import com.jjg.game.common.constant.MessageConst;
 import com.jjg.game.common.proto.ProtoDesc;
 import com.jjg.game.common.proto.ProtobufMessage;
-import com.jjg.game.core.pb.AbstractNotice;
-import com.jjg.game.table.common.message.TableRoomMessageConstant;
 
 /**
- * 通知押注类房间长时间未操作退出提示
+ * 通知房间退出提示
  *
  * @author 2CL
  */
-@ProtoDesc("通知押注类房间长时间未操作退出的提示")
+@ProtoDesc("通知房间退出的提示")
 @ProtobufMessage(
     messageType = MessageConst.MessageTypeDef.BET_GENERAL_TYPE,
-    cmd = TableRoomMessageConstant.RespMsgBean.NOTIFY_TABLE_EXIT_ROOM,
+    cmd = MessageConst.CoreMessage.NOTIFY_TABLE_EXIT_ROOM,
     resp = true
 )
 public class NotifyTableExitRoom extends AbstractNotice {
