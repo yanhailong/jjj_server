46d823a4d8f6d79689140e5e4ce89642ec612a92
chengchunlin <Ccl123!@#>
2025-09-11 14:51:37 +0800

    fix: 房间内货币获取调整

--- poker/src/main/java/com/jjg/game/poker/game/texas/room/TexasGameController.java
+++ poker/src/main/java/com/jjg/game/poker/game/texas/room/TexasGameController.java
@@ -552,7 +552,7 @@ public class TexasGameController extends BasePokerGameController<TexasGameDataVo
         TexasCfg texasCfg = TexasDataHelper.getTexasCfg(gameDataVo);
         if (hasTempCurrency < texasCfg.getTablecoin()) {
             long defaultCoinsNum = Math.min(TexasDataHelper.getDefaultCoinsNum(gameDataVo), texasCfg.getTablecoin());
-            if (gamePlayer.getGold() >= defaultCoinsNum) {
+            if (getItemNum(gamePlayer.getId()) >= defaultCoinsNum) {
                 //增加零时货币
                 gameDataVo.getTempGold().put(seatInfo.getPlayerId(), defaultCoinsNum);
                 NotifyTexasTempGoldReflush reflush = new NotifyTexasTempGoldReflush();
