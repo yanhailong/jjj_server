4869cc35b52c371a59222b48504c95fac6751562
why <Why123!@#>
2025-10-27 17:34:31 +0800

    新增本地调用支持，优先尝试本地服务调用以减少序列化和网络开销；重构积分服务实现，将 IPlayerPointsAwardService 替换为 RPC 方式的 HallPointsAwardBridge，优化积分逻辑调用路径。

---
+++ core/src/main/java/com/jjg/game/core/rpc/HallPointsAwardBridge.java
@@ -0,0 +1,25 @@
+package com.jjg.game.core.rpc;
+
+import com.jjg.game.common.rpc.IGameRpc;
+import com.jjg.game.core.constant.PointsAwardType;
+
+public interface HallPointsAwardBridge extends IGameRpc {
+
+    /**
+     * 添加积分
+     *
+     * @param playerId    玩家id
+     * @param pointsAward 增加的积分
+     * @param type        {@link PointsAwardType}
+     */
+    void add(long playerId, int pointsAward, int type);
+
+    /**
+     * 扣除积分
+     *
+     * @param playerId    玩家id
+     * @param pointsAward 扣除的积分 只支持正数
+     */
+    boolean deduct(long playerId, int pointsAward, int type);
+
+}
