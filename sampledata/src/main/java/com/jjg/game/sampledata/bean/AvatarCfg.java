457b878228d2c68f32c4b144d40c92c2714d530f
lm <1343955662@qq.com>
2025-11-04 09:52:49 +0800

    1.配置更新

--- sampledata/src/main/java/com/jjg/game/sampledata/bean/AvatarCfg.java
+++ sampledata/src/main/java/com/jjg/game/sampledata/bean/AvatarCfg.java
@@ -1,9 +1,7 @@
 package com.jjg.game.sampledata.bean;
 
-import java.util.*;
-
-
 import javax.annotation.processing.Generated;
+import java.util.List;
 /**
  * 配置bean
  *
@@ -21,28 +19,44 @@ public class AvatarCfg extends BaseCfgBean {
 
   /** 资源描述 */
   protected int Dec;
-  /** 客户端展示获得VIP条件 */
-  protected int VIPLevel;
+  /** 货币购买 */
+  protected List<Integer> buyItem;
+  /** 多语言ID */
+  protected int languageId;
   /** 资源名称 */
   protected int name;
+  /** 等级 */
+  protected int playerLv;
   /** 美术资源名称与路径 */
   protected String resourcePath;
   /** 类型 */
   protected int resourceType;
+  /** VIP条件 */
+  protected int vipLevel;
 
   /** 返回资源描述 */
   public int getDec() {
     return Dec;
   }
 
-  /** 返回客户端展示获得VIP条件 */
-  public int getVIPLevel() {
-    return VIPLevel;
+  /** 返回货币购买 */
+  public List<Integer> getBuyItem() {
+    return buyItem;
+  }
+
+  /** 返回多语言ID */
+  public int getLanguageId() {
+    return languageId;
   }
 
   /** 返回资源名称 */
   public int getName() {
     return name;
+  }
+
+  /** 返回等级 */
+  public int getPlayerLv() {
+    return playerLv;
   }
 
   /** 返回美术资源名称与路径 */
@@ -53,6 +67,11 @@ public class AvatarCfg extends BaseCfgBean {
   /** 返回类型 */
   public int getResourceType() {
     return resourceType;
+  }
+
+  /** 返回VIP条件 */
+  public int getVipLevel() {
+    return vipLevel;
   }
 
   @Override
