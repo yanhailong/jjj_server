b331c24cbf963a90dc627544279ae11e9c6a9b07
lm <1343955662@qq.com>
2025-11-14 10:06:29 +0800

    1.vip 修改

--- core/src/main/java/com/jjg/game/core/service/ShopService.java
+++ core/src/main/java/com/jjg/game/core/service/ShopService.java
@@ -304,8 +304,8 @@ public class ShopService implements OrderGenerate, GameEventListener {
                                 List<LanguageParamData> languageParamData = new ArrayList<>();
                                 languageParamData.add(new LanguageParamData(0, shopProduct.getMoney().toPlainString()));
                                 languageParamData.add(new LanguageParamData(0, String.valueOf(player.getVipLevel())));
-                                languageParamData.add(new LanguageParamData(0, String.valueOf(NumberUtil.decimalFormat(",##0", addNum))));
                                 languageParamData.add(new LanguageParamData(0, NumberUtil.decimalFormat("#.##%", BigDecimal.valueOf(add).divide(BigDecimal.valueOf(10000),4, RoundingMode.DOWN))));
+                                languageParamData.add(new LanguageParamData(0, String.valueOf(NumberUtil.decimalFormat(",##0", addNum))));
                                 mailService.addCfgMail(player.getId(), mailId, List.of(new Item(currencyItemId, addNum.longValue())), languageParamData);
                             }
                         }
