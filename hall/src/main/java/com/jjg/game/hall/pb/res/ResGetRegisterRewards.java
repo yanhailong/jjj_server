0baa813307050586cd6a0cba8438726b36f38d4b
lm <1343955662@qq.com>
2025-11-14 16:17:39 +0800

    1.协议修改

--- hall/src/main/java/com/jjg/game/hall/pb/res/ResGetRegisterRewards.java
+++ hall/src/main/java/com/jjg/game/hall/pb/res/ResGetRegisterRewards.java
@@ -10,7 +10,7 @@ import com.jjg.game.hall.constant.HallConstant;
  * @author 11
  * @date 2025/8/13 11:12
  */
-@ProtobufMessage(messageType = MessageConst.MessageTypeDef.HALL_TYPE, cmd = HallConstant.MsgBean.RES_GET_REGISTER_REWARDS)
+@ProtobufMessage(messageType = MessageConst.MessageTypeDef.HALL_TYPE, cmd = HallConstant.MsgBean.RES_GET_REGISTER_REWARDS,resp = true)
 @ProtoDesc("请求领取注册奖励")
 public class ResGetRegisterRewards extends AbstractResponse {
     public ResGetRegisterRewards(int code) {
