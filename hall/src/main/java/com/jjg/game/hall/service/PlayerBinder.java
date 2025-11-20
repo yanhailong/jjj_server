15e52d0a64c9cb965b38f959a996419091b57513
11 <11@qq.com>
2025-11-20 10:22:55 +0800

    session bug

--- hall/src/main/java/com/jjg/game/hall/service/PlayerBinder.java
+++ hall/src/main/java/com/jjg/game/hall/service/PlayerBinder.java
@@ -4,8 +4,6 @@ import com.jjg.game.common.listener.SessionReferenceBinder;
 import com.jjg.game.common.protostuff.PFSession;
 import com.jjg.game.core.data.Player;
 import com.jjg.game.core.data.PlayerController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +13,6 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class PlayerBinder implements SessionReferenceBinder {
-    private static final Logger log = LoggerFactory.getLogger(PlayerBinder.class);
     @Autowired
     private HallPlayerService hallPlayerService;
 
@@ -27,7 +24,6 @@ public class PlayerBinder implements SessionReferenceBinder {
         Player player = hallPlayerService.get(playerId);
         PlayerController playerController = new PlayerController(session, player);
         session.setReference(playerController);
-        log.info("获取playerId: " + playerId);
         return playerController;
     }
 }
