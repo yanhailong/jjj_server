4bffe53e8f83837a9916b1c931e709b5d08f85e3
11 <Lh123!@#rfv>
2025-12-25 14:35:16 +0800

    所有slots统一在离线30秒后处理离线事件

--- slots/src/main/java/com/jjg/game/slots/game/dollarexpress/manager/AbstractDollarExpressGameManager.java
+++ slots/src/main/java/com/jjg/game/slots/game/dollarexpress/manager/AbstractDollarExpressGameManager.java
@@ -1077,38 +1077,6 @@ public abstract class AbstractDollarExpressGameManager extends AbstractSlotsGame
         return 0;
     }
 
-    /**
-     * 退出游戏
-     *
-     * @param playerController
-     * @param initiativeExit
-     * @return 返回值来标记是否可以进行断线重连
-     */
-    @Override
-    public DollarExpressPlayerGameData exit(PlayerController playerController, boolean initiativeExit) {
-        DollarExpressPlayerGameData playerGameData = getPlayerGameData(playerController);
-        if (playerGameData == null) {
-            return null;
-        }
-
-        long now =  System.currentTimeMillis();
-        playerGameData.setOfflineTime(now);
-        playerGameData.setOnline(false);
-
-        if (initiativeExit) {
-            //退出自动执行事件
-            onAutoExitAction(playerGameData);
-            //保存数据
-            offlineSaveGameDataDto(playerGameData);
-            removePlayerGameData(playerGameData.playerId(), playerGameData.getRoomCfgId());
-        }else {
-            //30秒之后执行事件
-            OffLineEventData offLineEventData = new OffLineEventData(1,now + 30 * TimeHelper.ONE_SECOND_OF_MILLIS);
-            playerGameData.addOffLineEvent(offLineEventData);
-        }
-        return playerGameData;
-    }
-
     @Override
     protected void onAutoExitAction(DollarExpressPlayerGameData playerGameData) {
         if (playerGameData.getInvers().get()) {
