5671482becbc4f63f83dfbeefdae581e91bac3b3
lihaocao <lihaocao@qq.com>
2025-12-29 14:36:27 +0800

    功能解锁 GameFunction 增加vip等级解锁 修改常量

---
+++ core/src/main/java/com/jjg/game/core/constant/GameFunctionConstant.java
@@ -0,0 +1,24 @@
+package com.jjg.game.core.constant;
+
+import com.jjg.game.common.constant.MessageConst;
+import com.jjg.game.sampledata.bean.TaskCfg;
+
+/**
+ * 功能解锁
+ */
+public interface GameFunctionConstant {
+
+
+    interface ConditionType {
+        /**
+         * 等级
+         */
+        int CONDITION_LEVEL = 1;
+        /**
+         * vip等级
+         */
+        int CONDITION_VIP_LEVEL = 3;
+    }
+
+
+}
