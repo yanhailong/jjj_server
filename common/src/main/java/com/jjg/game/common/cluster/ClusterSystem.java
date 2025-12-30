03d31a2d4cfc47032926946706ea17d800335a81
lm <1343955662@qq.com>
2025-12-30 18:23:08 +0800

    任务bug修改

--- common/src/main/java/com/jjg/game/common/cluster/ClusterSystem.java
+++ common/src/main/java/com/jjg/game/common/cluster/ClusterSystem.java
@@ -247,18 +247,16 @@ public class ClusterSystem implements MarsNodeListener, TimerListener<String>, O
         }
         log.info("切换节点，sessionId={},toNode={}", pfSession.sessionId(), marsNode.getNodePath());
         try {
-            onSwitchNodeAfter(pfSession);
-            log.info("切换节点准备完成，sessionId={},toNode={}", pfSession.sessionId(), marsNode.getNodePath());
+            onSwitchNodeBefore(pfSession);
             SwitchNodeMessage switchNodeMessage = new SwitchNodeMessage(pfSession.sessionId(), marsNode.getNodePath()
                     , pfSession.playerId);
             pfSession.send2Gate(switchNodeMessage);
-            log.info("切换节点消息发送完成，sessionId={},toNode={}", pfSession.sessionId(), marsNode.getNodePath());
         } catch (Exception e) {
             log.warn("节点切换异常", e);
         }
     }
 
-    private void onSwitchNodeAfter(PFSession pfSession) {
+    private void onSwitchNodeBefore(PFSession pfSession) {
         List<OnSwitchNode> gameSysInterface = SystemInterfaceHolder.getGameSysInterface(OnSwitchNode.class);
         if (CollectionUtil.isNotEmpty(gameSysInterface)) {
             for (OnSwitchNode event : gameSysInterface) {
