a5dd26b6644ba2f4d4e37306379f92709ae65774
lm <1343955662@qq.com>
2026-01-23 18:23:56 +0800

    1.德州好友房提示修改

--- poker/src/main/java/com/jjg/game/poker/game/texas/room/TexasFriendGameController.java
+++ poker/src/main/java/com/jjg/game/poker/game/texas/room/TexasFriendGameController.java
@@ -5,16 +5,20 @@ import com.jjg.game.common.utils.TimeHelper;
 import com.jjg.game.core.constant.AddType;
 import com.jjg.game.core.constant.EGameType;
 import com.jjg.game.core.dao.room.FriendRoomBillHistoryDao;
+import com.jjg.game.core.data.FriendRoom;
 import com.jjg.game.core.data.FriendRoomBillHistoryBean;
 import com.jjg.game.core.data.Room;
 import com.jjg.game.core.data.RoomType;
-import com.jjg.game.core.utils.CoreUtil;
+import com.jjg.game.room.constant.EGamePhase;
 import com.jjg.game.room.controller.AbstractRoomController;
 import com.jjg.game.room.controller.GameController;
 import com.jjg.game.room.data.room.FriendRoomBillHistoryHelper;
 import com.jjg.game.room.data.room.RoomBankerChangeParam;
 import com.jjg.game.room.data.room.SettlementData;
 import com.jjg.game.room.friendroom.AbstractFriendRoomController;
+import com.jjg.game.room.friendroom.FriendRoomSampleUtils;
+import com.jjg.game.room.message.RoomMessageBuilder;
+import com.jjg.game.room.message.resp.NotifyPauseGameOnNewRound;
 import com.jjg.game.sampledata.bean.Room_ChessCfg;
 
 import java.util.HashMap;
@@ -43,6 +47,48 @@ public class TexasFriendGameController extends TexasGameController {
     }
 
     @Override
+    public void onDestroyRoomAction() {
+        if (getCurrentGamePhase() == EGamePhase.WAIT_READY) {
+            broadcastGamePauseInfo();
+        }
+    }
+
+    @Override
+    public void onContinueGameAction() {
+        if (getCurrentGamePhase() == EGamePhase.WAIT_READY) {
+            broadcastGamePauseInfo();
+        }
+    }
+
+    @Override
+    public void pauseGame() {
+        super.pauseGame();
+        if (getCurrentGamePhase() == EGamePhase.WAIT_READY) {
+            broadcastGamePauseInfo();
+        }
+    }
+
+    @Override
+    public void broadcastGamePauseInfo() {
+        NotifyPauseGameOnNewRound notifyPauseGameOnNewRound = new NotifyPauseGameOnNewRound();
+        FriendRoom friendRoom = getRoom();
+        if (friendRoom.getStatus() == 2) {
+            notifyPauseGameOnNewRound.pauseType = 1;
+        }
+        if (friendRoom.getOverdueTime() < System.currentTimeMillis()) {
+            notifyPauseGameOnNewRound.pauseType = 3;
+        }
+        int minBankerAmount = FriendRoomSampleUtils.getRoomMinBankerAmount(gameDataVo.getRoomCfg().getId());
+        if (friendRoom.getPredictCostGoldNum() < minBankerAmount) {
+            notifyPauseGameOnNewRound.pauseType = 2;
+        }
+        if (friendRoom.getStatus() == 3) {
+            notifyPauseGameOnNewRound.pauseType = 4;
+        }
+        broadcastToPlayers(RoomMessageBuilder.newBuilder().setData(notifyPauseGameOnNewRound).toAllPlayer());
+    }
+
+    @Override
     public void dealBankerFlowing(RoomBankerChangeParam param, Map<Long, SettlementData> settlementDataMap) {
         if (roomController instanceof AbstractFriendRoomController<?, ?>) {
             // 需要记录
@@ -53,24 +99,24 @@ public class TexasFriendGameController extends TexasGameController {
                 FriendRoomBillHistoryBean historyBean = FriendRoomBillHistoryHelper.buildFriendRoom(getRoom());
                 historyBean.setTotalIncome(roomCreatorTotalIncome);
                 long totalFlowing =
-                    settlementDataMap.values().stream()
-                        .filter(s -> s.getBetWin() > 0)
-                        .mapToLong(SettlementData::getBetWin)
-                        .sum();
+                        settlementDataMap.values().stream()
+                                .filter(s -> s.getBetWin() > 0)
+                                .mapToLong(SettlementData::getBetWin)
+                                .sum();
                 historyBean.setTotalFlowing(totalFlowing);
                 historyBean.setItemId(getGameTransactionItemId());
                 historyBean.setGameMajorType(CommonUtil.getMajorTypeByGameType(roomController.getRoom().getGameType()));
                 historyBean.setMonth(TimeHelper.getMonthNumerical());
                 historyBean.setPartInPlayerIncome(
-                    settlementDataMap.entrySet().stream()
-                        .collect(HashMap::new,
-                            (map, e)
-                                -> map.put(
-                                e.getKey(),
-                                e.getValue().getBetWin() > 0
-                                    ? e.getValue().getBetWin()
-                                    : -1 * e.getValue().getBetTotal()),
-                            HashMap::putAll));
+                        settlementDataMap.entrySet().stream()
+                                .collect(HashMap::new,
+                                        (map, e)
+                                                -> map.put(
+                                                e.getKey(),
+                                                e.getValue().getBetWin() > 0
+                                                        ? e.getValue().getBetWin()
+                                                        : -1 * e.getValue().getBetTotal()),
+                                        HashMap::putAll));
                 // 添加历史
                 dao.addFriendRoomBillHistory(historyBean);
             }
