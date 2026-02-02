ee3393d0d8433e5f599f24eeb628d2915a6f1155
lm <1343955662@qq.com>
2026-02-02 18:07:43 +0800

    象财神 bug修复

--- slots/src/main/java/com/jjg/game/slots/game/elephantgod/manager/ElephantGodGameManager.java
+++ slots/src/main/java/com/jjg/game/slots/game/elephantgod/manager/ElephantGodGameManager.java
@@ -1,12 +1,15 @@
 package com.jjg.game.slots.game.elephantgod.manager;
 
+import com.jjg.game.slots.game.elephantgod.dao.ElephantGodGameDataDao;
+import com.jjg.game.slots.game.elephantgod.dao.ElephantGodResultLibDao;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ElephantGodGameManager extends AbstractElephantGodGameManager{
-    public ElephantGodGameManager() {
-        super();
+    public ElephantGodGameManager(ElephantGodResultLibDao libDao, ElephantGodGenerateManager generateManager, ElephantGodGameDataDao gameDataDao) {
+        super(libDao, generateManager, gameDataDao);
         this.log = LoggerFactory.getLogger(getClass());
+
     }
 }
--- slots/src/main/java/com/jjg/game/slots/game/elephantgod/manager/ElephantGodRoomGameManager.java
+++ slots/src/main/java/com/jjg/game/slots/game/elephantgod/manager/ElephantGodRoomGameManager.java
@@ -1,13 +1,16 @@
 package com.jjg.game.slots.game.elephantgod.manager;
 
 import com.jjg.game.core.data.RoomType;
+import com.jjg.game.slots.game.elephantgod.dao.ElephantGodGameDataDao;
+import com.jjg.game.slots.game.elephantgod.dao.ElephantGodResultLibDao;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ElephantGodRoomGameManager extends AbstractElephantGodGameManager{
-    public ElephantGodRoomGameManager() {
-        super();
+
+    public ElephantGodRoomGameManager(ElephantGodResultLibDao libDao, ElephantGodGenerateManager generateManager, ElephantGodGameDataDao gameDataDao) {
+        super(libDao, generateManager, gameDataDao);
         this.log = LoggerFactory.getLogger(getClass());
     }
 
