091eed77b916bf202908beb8af77ed33c8407a5c
lm <1343955662@qq.com>
2026-02-03 13:35:41 +0800

    恶魔之子bug修复

--- slots/src/main/java/com/jjg/game/slots/game/demonchild/pb/handler/DemonChildMessageHandler.java
+++ slots/src/main/java/com/jjg/game/slots/game/demonchild/pb/handler/DemonChildMessageHandler.java
@@ -23,7 +23,7 @@ import org.springframework.stereotype.Component;
  * @date 2025/8/1 17:37
  */
 @Component
-@MessageType(MessageConst.MessageTypeDef.CAPTAIN_JACK)
+@MessageType(MessageConst.MessageTypeDef.DEMON_CHILD)
 public class DemonChildMessageHandler {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
