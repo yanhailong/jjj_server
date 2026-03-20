c9964900c9fc3f82c136de29cfb6fdfacbad4910
11 <Lh123!@#rfv>
2026-03-20 18:25:07 +0800

    添加设备类型

--- core/src/main/java/com/jjg/game/core/data/DeviceType.java
+++ core/src/main/java/com/jjg/game/core/data/DeviceType.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 
 /**
  * 设备类型
+ *
  * @author 11
  * @date 2025/10/31 13:48
  */
 public enum DeviceType {
     ANDROID(1),
     IOS(2),
-
-    ;
+    WEB(3);
 
     private int value;
 
