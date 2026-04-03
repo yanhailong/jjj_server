f46d9a44bd7d285923d8dbfaccc3ec0bed16686c
lm <1343955662@qq.com>
2026-04-03 09:05:12 +0800

    日志修改

--- slots/src/main/java/com/jjg/game/slots/game/superstar/manager/AbstractSuperStarGameManager.java
+++ slots/src/main/java/com/jjg/game/slots/game/superstar/manager/AbstractSuperStarGameManager.java
@@ -6,9 +6,6 @@ import com.jjg.game.core.data.Player;
 import com.jjg.game.core.data.PlayerController;
 import com.jjg.game.sampledata.GameDataManager;
 import com.jjg.game.sampledata.bean.WarehouseCfg;
-import com.jjg.game.slots.data.BetDivideInfo;
-import com.jjg.game.slots.data.SlotsPlayerGameDataDTO;
-import com.jjg.game.slots.game.superstar.SuperStarConstant;
 import com.jjg.game.slots.data.SlotsPlayerGameDataDTO;
 import com.jjg.game.slots.game.superstar.dao.SuperStarGameDataDao;
 import com.jjg.game.slots.game.superstar.dao.SuperStarResultLibDao;
@@ -75,7 +72,10 @@ public abstract class AbstractSuperStarGameManager extends AbstractSlotsGameMana
         SuperStarGameRunInfo gameRunInfo = new SuperStarGameRunInfo(Code.SUCCESS, playerGameData.playerId());
         try {
             gameRunInfo.setAuto(auto);
-            WarehouseCfg warehouseCfg = GameDataManager.getWarehouseCfg(playerController.getPlayer().getRoomCfgId());
+            Player player = slotsPlayerService.get(playerGameData.playerId());
+            playerController.setPlayer(player);
+            WarehouseCfg warehouseCfg = GameDataManager.getWarehouseCfg(player.getRoomCfgId());
+            gameRunInfo.setBeforeGold(getMoneyByItemId(warehouseCfg, player));
 
             gameRunInfo = normal(gameRunInfo, playerGameData, betValue);
 
@@ -97,7 +97,7 @@ public abstract class AbstractSuperStarGameManager extends AbstractSlotsGameMana
             gameRunInfo.setBigShowId(getBigShowIdByTimes(times));
             checkMarquee(playerGameData, gameRunInfo.getAllWinGold());
             //玩家当前金币
-            Player player = slotsPlayerService.get(playerGameData.playerId());
+            player = slotsPlayerService.get(playerGameData.playerId());
             playerController.setPlayer(player);
 
             gameRunInfo.setAfterGold(getMoneyByItemId(warehouseCfg, player));
