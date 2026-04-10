503b22d0dc5cdf228fe4cade365da8ebb66ad3e8
lihaocao <lihaocao@qq.com>
2026-04-10 09:22:30 +0800

    放开kafka 消息

--- core/src/main/java/com/jjg/game/core/logger/BaseLogger.java
+++ core/src/main/java/com/jjg/game/core/logger/BaseLogger.java
@@ -509,7 +509,7 @@ public class BaseLogger {
         String msg = JSONObject.toJSONString(json, SerializerFeature.WriteNonStringKeyAsString);
 
         topic = StringUtils.isEmpty(topic) ? GAME_LOGS_TOPIC : topic.toLowerCase();
-//        kafkaTemplate.send(topic, RandomUtils.getUUid(), msg);
+        kafkaTemplate.send(topic, RandomUtils.getUUid(), msg);
 
 //        log.debug("打印日志数据 topic = {},msg = {}", topic, msg);
     }
