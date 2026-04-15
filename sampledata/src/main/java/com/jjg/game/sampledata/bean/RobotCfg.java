14ccd4e31f3e3a267badd2e70e6d0aded08315dd
lihaocao <lihaocao@qq.com>
2026-04-15 11:03:36 +0800

    南方前进 机器人出牌逻辑修改

--- sampledata/src/main/java/com/jjg/game/sampledata/bean/RobotCfg.java
+++ sampledata/src/main/java/com/jjg/game/sampledata/bean/RobotCfg.java
@@ -27,6 +27,8 @@ public class RobotCfg extends BaseCfgBean {
   protected List<List<Integer>> betRobotID;
   /** 对战类行为策略ID-德州 */
   protected List<List<Integer>> chessRobotID;
+  /** 对战类行为策略ID-南方前进 */
+  protected List<List<Integer>> southRobotID;
   /** 押注类-每次局游戏结束时退出概率 */
   protected int exit;
   /** 押注类-退出房间时倍数 */
@@ -111,6 +113,11 @@ public class RobotCfg extends BaseCfgBean {
   /** 返回玩家等级 */
   public int getPlayerLevel() {
     return playerLevel;
+  }
+
+  /** 返回对战类行为策略ID-南方前进 */
+  public List<List<Integer>> getSouthRobotID() {
+    return southRobotID;
   }
 
   /** 返回VIP等级 */
