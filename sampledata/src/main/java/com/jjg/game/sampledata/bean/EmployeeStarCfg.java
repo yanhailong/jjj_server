7fb4fc4901be7bca79c1aaeee64fd912def20b41
11 <123456@gmail.com>
2026-07-24 10:21:52 +0800

    更新配置

--- sampledata/src/main/java/com/jjg/game/sampledata/bean/EmployeeStarCfg.java
+++ sampledata/src/main/java/com/jjg/game/sampledata/bean/EmployeeStarCfg.java
@@ -27,6 +27,8 @@ public class EmployeeStarCfg extends BaseCfgBean {
   protected int Star;
   /** 升星碎片消耗 */
   protected int StarUpCost;
+  /** 主管加成 */
+  protected Map<Integer,Integer> SupervisorBonus;
 
   /** 返回雇员ID */
   public int getEmployeeID() {
@@ -46,6 +48,11 @@ public class EmployeeStarCfg extends BaseCfgBean {
   /** 返回升星碎片消耗 */
   public int getStarUpCost() {
     return StarUpCost;
+  }
+
+  /** 返回主管加成 */
+  public Map<Integer,Integer> getSupervisorBonus() {
+    return SupervisorBonus;
   }
 
   @Override
